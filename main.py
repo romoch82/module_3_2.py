3f7d0fa81c327e2b8a5c68380d33535a268ca0d0
romoch82 <romoch37@gmail.com>
2024-11-01 22:15:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def send_email(message, recipient,sender = "university.help@gmail.com"):
-    if ("@" and (".com" or ".ru" or ".net")) not in (recipient or sender) or ("@" or (".com" or ".ru" or ".net")) not in (recipient or sender):
-        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-    elif recipient == sender:
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    if '@' not in recipient or not recipient.endswith(
+            ('.com', '.ru', '.net')) or '@' not in sender or not sender.endswith(('.com', '.ru', '.net')):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
+    elif sender == recipient:
         print("Нельзя отправить письмо самому себе!")
     elif sender == "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
